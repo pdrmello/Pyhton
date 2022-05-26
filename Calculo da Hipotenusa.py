6cfa6b7e0ab7a5b56ea0046dae2558eac33bbcab
Pedro <pedrosamello@hotmail.com>
2022-05-26 11:58:23 -0300

    Primeiro Commit

---
+++ Calculo da Hipotenusa.py
@@ -0,0 +1,6 @@
+import math
+print("DESAFIO 17")
+co = float(input("Digite o valor do cateto oposto: "))
+ca = float(input("Digite o valor do catato adjacente: "))
+h = math.sqrt(math.pow(co,2) + math.pow(ca,2)) 
+print(f"O resultado da hipotesnusa é {h}")
